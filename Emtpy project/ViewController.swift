cf6025e794ade21f32b74daccdf077688687669a
s.hitipeuw <werksimon@gmail.com>
2019-05-17 11:02:49 +0200

    Initial Commit

---
+++ Emtpy project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Emtpy project
+//
+//  Created by Simon Hitipeuw on 17/05/2019.
+//  Copyright © 2019 Simon Hitipeuw. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
